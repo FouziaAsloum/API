21c03e296923ce8b7109769137defb6a6642dd13
SanaSanss <fouzia.asloum@outlook.fr>
2023-07-18 11:58:38 +0200

    1st

--- Consommer-une-api/Consommer-une-api/js/script.js
+++ Consommer-une-api/Consommer-une-api/js/script.js
@@ -1,2 +1,73 @@
-// Votre code Javascript ici
-"J'écris du js"
+document.addEventListener('DOMContentLoaded', () => {
+  const userList = document.getElementById('user-list');
+  const userModal = document.getElementById('user-modal');
+  const userModalContent = document.getElementById('user-modal-content');
+
+  // Fonction pour récupérer les données de l'API
+  async function fetchUsers() {
+    try {
+      const response = await fetch('api.json'); // Charger les données à partir du fichier api.json
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Erreur de récupération des utilisateurs:', error);
+      return [];
+    }
+  }
+
+  // Fonction pour afficher les informations détaillées d'un utilisateur dans la div modale
+  function showUserDetails(user) {
+    userModalContent.innerHTML = `
+      <img src="${user.avatar}" alt="Avatar de ${user.first_name}">
+      <h2>${user.first_name} ${user.last_name}</h2>
+      <p><strong>Email:</strong> ${user.email}</p>
+      <p><strong>ID:</strong> ${user.id}</p>
+      <!-- Ajoutez d'autres propriétés ici si nécessaire -->
+    `;
+
+    userModal.style.display = 'flex';
+  }
+
+  // Fonction pour masquer la div modale
+  function hideUserModal() {
+    userModal.style.display = 'none';
+  }
+
+  // Fonction pour afficher les utilisateurs sur la page
+  async function displayUsers() {
+    const users = await fetchUsers();
+    if (users.length === 0) {
+      userList.innerHTML = '<li>Aucun utilisateur trouvé.</li>';
+      return;
+    }
+
+    userList.innerHTML = users.map(user => {
+      return `<li data-id="${user.id}">
+        <span>Nom:</span> ${user.first_name} ${user.last_name}<br>
+        <span>Email:</span> ${user.email}<br>
+        <img src="${user.avatar}" alt="Avatar de ${user.first_name}">
+      </li>`;
+    }).join('');
+
+    // Ajouter un gestionnaire d'événements pour afficher les détails de l'utilisateur au clic
+    userList.addEventListener('click', (event) => {
+      const targetUser = event.target.closest('li');
+      if (targetUser) {
+        const userId = targetUser.getAttribute('data-id');
+        const user = users.find(u => u.id === parseInt(userId));
+        if (user) {
+          showUserDetails(user);
+        }
+      }
+    });
+
+    // Ajouter un gestionnaire d'événements pour masquer la div modale au clic en dehors de celle-ci
+    userModal.addEventListener('click', (event) => {
+      if (event.target === userModal) {
+        hideUserModal();
+      }
+    });
+  }
+
+  displayUsers();
+});
